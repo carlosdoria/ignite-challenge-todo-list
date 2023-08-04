27ed5649e05ccea16e046310fccebff0eb54ae0a
Carlos Dória <carlosdoria953@gmail.com>
2023-08-03 22:49:32 -0300

    feat: add status bar

--- App.tsx
+++ App.tsx
@@ -1,5 +1,17 @@
+import { View } from "react-native";
 import { Home } from "./src/screen/home";
+import { THEME } from "./src/styles/theme";
+import { StatusBar } from "react-native";
 
 export default function App() {
-  return <Home />;
+  return (
+    <View style={{ flex: 1, backgroundColor: THEME.COLORS.GRAY_600 }}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Home />
+    </View>
+  );
 }
