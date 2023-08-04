6936cf78b82e6ff72b4a32dc6d5dddcce97212fd
Carlos Dória <carlosdoria953@gmail.com>
2023-08-03 22:48:01 -0300

    style: create theme

---
+++ src/styles/theme/index.ts
@@ -0,0 +1,25 @@
+export const THEME = {
+  COLORS: {
+    BLUE_DARK: "#1E6F9F",
+    BLUE: "#4EA8DE",
+    PURPLE_DARK: "#5E60CE",
+    PURPLE: "#8284FA",
+    DANGER: "#E25858",
+    GRAY_100: "#F2F2F2",
+    GRAY_200: "#D9D9D9",
+    GRAY_300: "#808080",
+    GRAY_400: "#333333",
+    GRAY_500: "#262626",
+    GRAY_600: "#1A1A1A",
+    GRAY_700: "#0D0D0D",
+  },
+  FONT_SIZE: {
+    SM: 12,
+    MD: 14,
+    LG: 16,
+  },
+  FONT_FAMILY: {
+    REGULAR: "Roboto_400Regular",
+    BOLD: "Roboto_700Bold",
+  },
+};
