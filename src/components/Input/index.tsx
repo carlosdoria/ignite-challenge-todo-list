a878f175436dbde1088eb4fd711063f72719a466
Carlos Dória <carlosdoria953@gmail.com>
2023-08-05 16:52:55 -0300

    feat: add props

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,17 +1,27 @@
-import { View, Text, TextInput } from "react-native";
+import { View, Text, TextInput, TouchableOpacity } from "react-native";
 import { styles } from "./styles";
 import { THEME } from "../../styles/theme";
 
-type InputProps = {};
+type InputProps = {
+  value: string;
+  setValue: (value: string) => void;
+  onSubmit: () => void;
+};
 
-export function Input({}: InputProps) {
+export function Input({ value, setValue, onSubmit }: InputProps) {
   return (
     <View style={styles.container}>
       <TextInput
+        value={value}
+        onChangeText={setValue}
         placeholder="Adicione uma nova tarefa"
         placeholderTextColor={THEME.COLORS.GRAY_300}
         style={styles.input}
       />
+
+      <TouchableOpacity style={styles.button} onPress={onSubmit}>
+        <Text>+</Text>
+      </TouchableOpacity>
     </View>
   );
 }
--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -4,13 +4,32 @@ import { THEME } from "../../styles/theme";
 export const styles = StyleSheet.create({
   container: {
     width: "100%",
+    marginTop: -26,
     paddingHorizontal: 24,
+
+    flexDirection: "row",
   },
   input: {
+    height: 52,
+    marginRight: 4,
     padding: 16,
-    backgroundColor: THEME.COLORS.GRAY_500,
+
+    flex: 1,
+
     color: THEME.COLORS.GRAY_100,
     fontSize: THEME.FONT_SIZE.LG,
-    borderRadius: 6,
+
+    borderRadius: THEME.BORDER_RADIUS,
+    backgroundColor: THEME.COLORS.GRAY_500,
+  },
+  button: {
+    height: 52,
+    width: 52,
+
+    alignItems: "center",
+    justifyContent: "center",
+
+    backgroundColor: THEME.COLORS.BLUE_DARK,
+    borderRadius: THEME.BORDER_RADIUS,
   },
 });
