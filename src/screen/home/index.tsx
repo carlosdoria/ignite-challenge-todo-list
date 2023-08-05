29c95a318e7b1ade0f4df856531f30f5e648aa5d
Carlos Dória <carlosdoria953@gmail.com>
2023-08-05 16:53:21 -0300

    feat: create props to input

--- src/screen/home/index.tsx
+++ src/screen/home/index.tsx
@@ -1,14 +1,40 @@
-import { View, Text, SafeAreaView } from "react-native";
+import { View, Text, SafeAreaView, Alert } from "react-native";
 import { Header } from "../../components/Header";
 import { Input } from "../../components/Input";
+import { useState } from "react";
 
 type screenProps = {};
 
 export function Home({}: screenProps) {
+  const [newTaskState, setNewTaskState] = useState("");
+  const [tasksState, setTasksState] = useState<String[]>([]);
+
+  const handleAddNewTask = () => {
+    if (newTaskState.length < 3) {
+      Alert.alert("Digite um nome válido.", "O texto é muito pequeno.");
+      return;
+    }
+
+    try {
+      setTasksState([...tasksState, newTaskState]);
+      setNewTaskState("");
+
+      Alert.alert("Tarefa.", "Tarefa adicionada com sucesso.");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <SafeAreaView>
       <Header />
-      <Input />
+      <Input
+        value={newTaskState}
+        setValue={setNewTaskState}
+        onSubmit={handleAddNewTask}
+      />
+
+      {/* <TaskList/> */}
     </SafeAreaView>
   );
 }
